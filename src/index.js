3a7b65252e4c94ab3bad9753a1becf5bc8e57fa2
Carolina Picetti <carolinapicetti@gmail.com>
2025-09-26 17:57:58 +0200

    Added the API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-function generatePoem(event){
-event.preventDefault();
-
-new Typewriter("#poem", {
-  strings: "poema aqui",
+function displayPoem(response){
+    new Typewriter("#poem", {
+  strings: response.data.answer,
   autoStart: true,
   delay: 1,
   cursor: "",
 });
+}
+
+
+
+function generatePoem(event){
+event.preventDefault();
+
+let instructionsInput = document.querySelector("#user-instructions")
+let apiKey = "141903t193o7f25eb4f8b827eac040ff";
+let prompt = `User instructions are: Generate a poem about ${instructionsInput.value}`;
+let context= "You are an excellent poet and love writing short poems, you need to generate a poem with maximum 8 lines in basic HTML, you don't need to show the word HTML, you need to follow the user instructions, remember to always put a tittle in your poem";
+let apiUrl= `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+axios.get(apiUrl).then(displayPoem);
+
+
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form")
